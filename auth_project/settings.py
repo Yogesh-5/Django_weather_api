d1f128d2c7f1d416d201197f572c1bf614bb7add
yogesh-5 <yogeshsoni2555@gmail.com>
2025-02-13 13:08:16 +0530

    initial commit

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-lnwg7ei6fuh#-hdk!32)(hk2liupo!6^r(tw6_!0e%$#&!gxwx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
